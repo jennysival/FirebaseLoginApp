20c7453ace27351ee6bde959fb513c232491b0ee
jennysival <jeniffersival@gmail.com>
2022-07-14 23:26:13 -0300

    Adicionar fragmento de navegação à home

--- app/src/main/java/br/com/zup/firebaselogin/ui/main/view/MainActivity.kt
+++ app/src/main/java/br/com/zup/firebaselogin/ui/main/view/MainActivity.kt
@@ -2,11 +2,18 @@ package br.com.zup.firebaselogin.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import br.com.zup.firebaselogin.R
+import br.com.zup.firebaselogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
     }
 }
