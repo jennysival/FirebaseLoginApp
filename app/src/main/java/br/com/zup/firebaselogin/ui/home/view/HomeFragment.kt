4a9343b306efda4352e472cfc41a61a490860302
jennysival <jeniffersival@gmail.com>
2022-07-15 00:16:35 -0300

    Adicionar função para exibir menu de logout

--- app/src/main/java/br/com/zup/firebaselogin/ui/home/view/HomeFragment.kt
+++ app/src/main/java/br/com/zup/firebaselogin/ui/home/view/HomeFragment.kt
@@ -26,6 +26,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
 
         showUserEmail()
 
