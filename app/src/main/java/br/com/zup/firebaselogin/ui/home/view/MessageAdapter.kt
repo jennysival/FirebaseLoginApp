e3c50b71f176c36c4fd43a584469fdd7f408d869
jennysival <jeniffersival@gmail.com>
2022-07-15 23:28:37 -0300

    Adicionar adapter

--- app/src/main/java/br/com/zup/firebaselogin/ui/home/view/MessageAdapter.kt
+++ app/src/main/java/br/com/zup/firebaselogin/ui/home/view/MessageAdapter.kt
@@ -1,4 +1,37 @@
 package br.com.zup.firebaselogin.ui.home.view
 
-class MessageAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.zup.firebaselogin.databinding.MessageItemBinding
+
+class MessageAdapter(
+    private var messageList: MutableList<String>
+    ): RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+        class ViewHolder(val binding: MessageItemBinding): RecyclerView.ViewHolder(binding.root){
+            fun showMessage(message: String){
+                binding.tvMsgCard.text = message
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = MessageItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val savedMessage = messageList[position]
+        holder.showMessage(savedMessage)
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
+    fun updateMessageList(newList: MutableList<String>){
+        messageList = newList
+        notifyDataSetChanged()
+    }
+
 }
