f427e73598b43ca49817edd1d29bcea9795cd534
jennysival <jeniffersival@gmail.com>
2022-07-15 23:41:17 -0300

    Implementar métodos de get da lista de mensagens

--- app/src/main/java/br/com/zup/firebaselogin/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/zup/firebaselogin/ui/home/viewmodel/HomeViewModel.kt
@@ -6,12 +6,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.firebaselogin.domain.repository.AuthRepository
 import br.com.zup.firebaselogin.domain.repository.MessageRepository
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class HomeViewModel: ViewModel() {
     private val authRepository = AuthRepository()
     private val messageRepository = MessageRepository()
 
-    private val _msgState = MutableLiveData<String>()
+    private var _messageListState = MutableLiveData<List<String>>()
+    val messageListState: LiveData<List<String>> = _messageListState
+
+    private var _msgState = MutableLiveData<String>()
     val msgState: LiveData<String> = _msgState
 
     fun getUserEmail() = authRepository.getUserEmail()
@@ -33,5 +39,23 @@ class HomeViewModel: ViewModel() {
     private fun getMessagePath(message: String): String?{
         val uri: Uri = Uri.parse(message)
         return uri.toString()
+    }
+
+    fun getSavedMessagesList(){
+        messageRepository.getSavedMessagesList()
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot){
+                    val messageList = mutableListOf<String>()
+                    for (resultSnapshot in snapshot.children){
+                        val messageResponse = resultSnapshot.getValue(String::class.java)
+                        messageResponse?.let { messageList.add(it) }
+                    }
+                    _messageListState.value = messageList
+                }
+                override fun onCancelled(error: DatabaseError){
+                    _msgState.value = error.message
+                }
+
+            })
     }
 }
