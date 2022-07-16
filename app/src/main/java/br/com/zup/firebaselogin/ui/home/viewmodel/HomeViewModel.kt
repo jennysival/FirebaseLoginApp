928789abfed77e0cf8296aa8d3263df610978855
jennysival <jeniffersival@gmail.com>
2022-07-15 23:08:03 -0300

    Implementar funções de mensagem no viewmodel da home

--- app/src/main/java/br/com/zup/firebaselogin/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/zup/firebaselogin/ui/home/viewmodel/HomeViewModel.kt
@@ -1,12 +1,40 @@
 package br.com.zup.firebaselogin.ui.home.viewmodel
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.firebaselogin.domain.repository.AuthRepository
+import br.com.zup.firebaselogin.domain.repository.MessageRepository
 
 class HomeViewModel: ViewModel() {
     private val authRepository = AuthRepository()
+    private val messageRepository = MessageRepository()
+
+    private val _messageResponse = MutableLiveData<String>()
+    val messageResponse: LiveData<String> = _messageResponse
+
+    private val _msgState = MutableLiveData<String>()
+    val msgState: LiveData<String> = _msgState
 
     fun getUserEmail() = authRepository.getUserEmail()
 
     fun logout() = authRepository.logout()
+
+    fun saveUserMessage(message: String){
+        val messagePath = getMessagePath(message)
+        messageRepository.databaseRef().child("$messagePath")
+            .setValue(message){
+                error,reference ->
+                if (error != null){
+                    _msgState.value = error.message
+                }
+                _msgState.value = "Mensagem salva!"
+            }
+    }
+
+    private fun getMessagePath(message: String): String?{
+        val uri: Uri = Uri.parse(message)
+        return uri.toString()
+    }
 }
