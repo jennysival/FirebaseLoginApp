421cd73dc4555af542715b2329e8c50fc875bff4
jennysival <jeniffersival@gmail.com>
2022-07-15 22:54:43 -0300

    Implementar repositório das mensagens

--- app/src/main/java/br/com/zup/firebaselogin/domain/repository/MessageRepository.kt
+++ app/src/main/java/br/com/zup/firebaselogin/domain/repository/MessageRepository.kt
@@ -1,4 +1,19 @@
 package br.com.zup.firebaselogin.domain.repository
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.ktx.Firebase
+
 class MessageRepository {
+    private val authentication: FirebaseAuth = Firebase.auth
+    private val database = FirebaseDatabase.getInstance()
+    private val reference = database.getReference("firebaseapplogin/${authentication.currentUser?.uid}/UserSavedMessages")
+
+    fun databaseRef() = reference
+
+    fun getSavedMessagesList(): Query{
+        return reference.orderByValue()
+    }
 }
